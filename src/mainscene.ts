765663b23e8587cb4a4fd6f851ac91a27c85c090
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-10-28 23:08:34 +0200

    Warpspeed removed.

--- src/mainscene.ts
+++ src/mainscene.ts
@@ -1,12 +1,15 @@
 import { ExtendedObject3D, Scene3D, THREE } from 'enable3d';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // @ts-ignore
 export default class MainScene extends Scene3D {
   audioContext: AudioContext | undefined;
 
+  cameraTarget: THREE.Vector3;
+
   orb: ExtendedObject3D | undefined;
 
-  pCamera: THREE.PerspectiveCamera | undefined;
+  orbitControls: OrbitControls | undefined;
 
   pointerCoords: THREE.Vector2;
 
@@ -18,18 +21,49 @@ export default class MainScene extends Scene3D {
 
   constructor() {
     super({ key: 'MainScene' });
+    this.cameraTarget = new THREE.Vector3();
     this.pointerCoords = new THREE.Vector2();
-    this.raycaster = new THREE.Raycaster();
     this.pointerdownRequest = false;
+    this.raycaster = new THREE.Raycaster();
     this.resizeRequest = false;
   }
 
   async create() {
-    await this.warpSpeed('-ground');
+    // const { orbitControls: oc } = await this.warpSpeed('-ground', '-light');
 
     // this.physics.debug?.enable();
 
-    this.pCamera = this.camera as THREE.PerspectiveCamera;
+    this.cameraTarget = new THREE.Vector3(0, 0, 0);
+
+    // CAMERA
+    this.camera = new THREE.PerspectiveCamera();
+    this.camera.position.set(0, 6, 10);
+    this.camera.lookAt(this.cameraTarget);
+    this.camera.updateProjectionMatrix();
+
+    // AMBIENT LIGHT
+    const ambientLight = new THREE.AmbientLight(0x0000ff, 0.35);
+    this.scene.add(ambientLight);
+
+    // DIRECTIONAL LIGHT
+    const SHADOW_MAP_SIZE = 2048;
+    const SHADOW_FAR = 13500;
+    const SHADOW_SIZE = 6;
+    const directionalLight = new THREE.DirectionalLight(0x330000, 0.5);
+    directionalLight.position.set(8, 10, 4);
+    directionalLight.color.setHSL(0.8, 1, 0.55);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = SHADOW_MAP_SIZE;
+    directionalLight.shadow.mapSize.height = SHADOW_MAP_SIZE;
+    directionalLight.shadow.camera.left = -SHADOW_SIZE;
+    directionalLight.shadow.camera.right = SHADOW_SIZE;
+    directionalLight.shadow.camera.top = SHADOW_SIZE;
+    directionalLight.shadow.camera.bottom = -SHADOW_SIZE;
+    directionalLight.shadow.camera.far = SHADOW_FAR;
+    this.scene.add(directionalLight);
+
+    this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+
     this.resizeRequest = true;
 
     const ground = this.physics.add.box(
@@ -105,7 +139,7 @@ export default class MainScene extends Scene3D {
 
   processClick() {
     this.pointerdownRequest = false;
-    if (!this.orb) {
+    if (!this.orb || !this.camera) {
       return;
     }
     this.raycaster.setFromCamera(this.pointerCoords, this.camera);
@@ -128,10 +162,19 @@ export default class MainScene extends Scene3D {
 
   resize() {
     this.resizeRequest = false;
-    if (this.pCamera) {
-      this.pCamera.aspect = window.innerWidth / window.innerHeight;
-      this.pCamera.updateProjectionMatrix();
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (this.camera) {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+
+      // move camera further back when viewport height increases so objects stay the same size
+      // const scale = 0.01;
+      // const fieldOfView = this.camera.fov * (Math.PI / 180); // convert fov to radians
+      // const targetZ = window.innerHeight / (2 * Math.tan(fieldOfView / 2));
+
+      // this.camera.position.z = targetZ * scale;
+      // this.camera.lookAt(this.cameraTarget);
     }
   }
 
